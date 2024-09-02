500ba0875593d07b55bcb4da53cf46a620695c2a
NagatsukiSep <nagamatsu.r.ab@m.titech.ac.jp>
2024-09-02 18:22:29 +0900

    fix

--- pages/api/getDeckData.ts
+++ pages/api/getDeckData.ts
@@ -2,8 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 // import puppeteer from 'puppeteer-core';
 // import chromium from '@sparticuz/chromium';
 
-let chromium: { args: any; executablePath: any; headless: any; };
-let puppeteer: { launch: (arg0: { args: any; executablePath: any; headless: any; }) => any; };
 
 interface Card {
   src: string;
@@ -12,12 +10,35 @@ interface Card {
 }
 
 const isLocal = !process.env.AWS_LAMBDA_FUNCTION_NAME;
-if (isLocal) {
-  puppeteer = require('puppeteer');
-}
-else {
-  chromium = require('@sparticuz/chromium');
-  puppeteer = require('puppeteer-core');
+
+const CHROMIUM_PATH = "https://vomrghiulbmrfvmhlflk.supabase.co/storage/v1/object/public/chromium-pack/chromium-v123.0.0-pack.tar";
+async function getBrowser() {
+  if (!isLocal) {
+    const chromium = await import("@sparticuz/chromium-min").then(
+      (mod) => mod.default
+    );
+
+    const puppeteerCore = await import("puppeteer-core").then(
+      (mod) => mod.default
+    );
+
+    const executablePath = await chromium.executablePath(CHROMIUM_PATH);
+
+    const browser = await puppeteerCore.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath,
+      headless: true,
+    });
+    return browser;
+  } else {
+    const browser = await require('puppeteer').launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: process.env.CHROMIUM_PATH,
+      headless: true,
+    });
+    return browser;
+  }
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -28,47 +49,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: 'No code provided' });
   }
 
-  let browser;
-  try {
-    if (isLocal) {
-      // ローカル環境でpuppeteerを使用
-      browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablePath: process.env.CHROMIUM_PATH,
-        headless: true,
-      });
-    } else {
-      // Vercel環境でchrome-aws-lambdaを使用
-      browser = await puppeteer.launch({
-        args: chromium.args,
-        executablePath: await await chromium.executablePath(
-          "https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar",
-        ),
-        headless: false,
-      });
-    }
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle2' });
+  const browser = await getBrowser();
 
-    const cards: Card[] = await page.evaluate(() => {
-      return Array.from(document.querySelectorAll('tr.imgBlockArea')).map(row => {
-        const imgElement = row.querySelector('img');
-        const spanElement = row.nextElementSibling?.querySelector('span');
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'networkidle2' });
 
-        return {
-          src: "https://www.pokemon-card.com" + imgElement?.getAttribute('src') || '',
-          name: imgElement?.getAttribute('alt') || '',
-          count: (spanElement?.textContent?.replace('枚', '').trim() || '0') as unknown as number,
-        };
-      });
+  const cards: Card[] = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('tr.imgBlockArea')).map(row => {
+      const imgElement = row.querySelector('img');
+      const spanElement = row.nextElementSibling?.querySelector('span');
+
+      return {
+        src: "https://www.pokemon-card.com" + imgElement?.getAttribute('src') || '',
+        name: imgElement?.getAttribute('alt') || '',
+        count: (spanElement?.textContent?.replace('枚', '').trim() || '0') as unknown as number,
+      };
     });
-    res.status(200).json(cards);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
-  } finally {
-    if (browser) {
-      await browser.close();
-    }
-  }
+  });
+  res.status(200).json(cards);
 }
